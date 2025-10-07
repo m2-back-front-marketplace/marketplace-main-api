a1dc246cf5ad01cd0b6469857e549b3e6224a331
Rémy <remy.berjonneau@gmail.com>
2025-10-07 16:31:38 +0200

    feat(init:husky,prettier,eslint)

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+export default {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'prettier', 'unused-imports'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  rules: {
+    'prettier/prettier': ['error'],
+    'no-console': 'warn',
+    'unused-imports/no-unused-imports': 'error',
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
+      { argsIgnorePattern: '^_' },
+    ],
+  },
+  ignorePatterns: ['dist', 'node_modules'],
+};
