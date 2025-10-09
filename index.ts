9151dcd2286a94fe4455dac0e580e44bf4f13e87
remy <remy.berjonneau@gmail.com>
2025-10-09 13:09:19 +0200

    feature / setup server + swagger

--- index.ts
+++ index.ts
@@ -1,21 +1,61 @@
-import { PrismaClient } from './generated/prisma';
+import Fastify from "fastify";
+import swagger from "@fastify/swagger";
+import swaggerUI from "@fastify/swagger-ui";
 
-const prisma = new PrismaClient();
 
-async function main() {
-  if ((await prisma.users.count()) < 1) {
-    await prisma.users.create({
-      data: {
-        username: 'test',
-        email: 'test@test.com',
-        password: 'test',
-        role: 'Client',
+const fastify = Fastify({
+  logger: true
+});
+
+fastify.register(swagger, {
+  swagger: {
+    info: {
+      title: "Documento API",
+      description: "API pour l'application Documento",
+      version: "1.0.0",
+    },
+    externalDocs: {
+      url: "https://swagger.io",
+      description: "Find more info here",
+    },
+    host: `${process.env.API_URL || "localhost"}:8000`,
+    schemes: ["http", "https"],
+    consumes: ["application/json"],
+    produces: ["application/json"],
+    tags: [
+      { name: "user", description: "User related endpoints" },
+    ],
+    securityDefinitions: {
+      cookieAuth: {
+        type: "apiKey",
+        name: "token",
+        in: "cookie",
       },
-    });
+    },
+  },
+});
+
+// Registrer Swagger UI
+fastify.register(swaggerUI, {
+  routePrefix: "/documentation",
+  uiConfig: {
+    docExpansion: "list",
+    deepLinking: false,
+  },
+});
+
+
+fastify.get("/health", async (request, reply) => {
+  reply.send({health: "api is up and healthy"});
+});
+
+const start = async () => {
+  try {
+    await fastify.listen({port: 8000});
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
   }
-  console.log(prisma.users.findMany());
-}
+};
 
-main();
-
-console.log('caca');
+await start();
