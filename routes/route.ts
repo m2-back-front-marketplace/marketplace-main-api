4eff1650ac1eb54a0668d15d96de223768562d02
remy <remy.berjonneau@gmail.com>
2025-11-18 11:05:26 +0100

    feature: add getCategory route and controller

--- routes/route.ts
+++ routes/route.ts
@@ -1,13 +1,17 @@
+import type { FastifyInstance } from "fastify";
+
 import userRoutes from "./userRoutes";
 import productRoutes from "./productRoutes";
 import cartRoutes from "./cartRoutes";
 import purchaseRoutes from "./purchaseRoutes";
+import categoryRoutes from "./categoryRoutes";
 
-const routes = async (fastify) => {
+const routes = async (fastify: FastifyInstance) => {
   fastify.register(userRoutes, { prefix: "/user" });
   fastify.register(productRoutes, { prefix: "/product" });
   fastify.register(cartRoutes, { prefix: "/cart" });
   fastify.register(purchaseRoutes, { prefix: "/purchase" });
+  fastify.register(categoryRoutes, { prefix: "/category" });
 };
 
 export default routes;
