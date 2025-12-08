a6aa92eda202ab3f9e1f65b286efd5661f77a014
remy <remy.berjonneau@gmail.com>
2025-12-08 12:49:40 +0100

    fix: change review pr

--- controllers/productsController.ts
+++ controllers/productsController.ts
@@ -213,7 +213,7 @@ const productsController = (prisma: PrismaClient) => ({
           },
         },
       });
-      return reply.status(200).send(product);
+      return reply.status(200).send({ data: product, message: "Product fetched successfully" });
     } catch (error) {
       console.error("Error while getting product:", error);
       return reply.status(500).send({ message: "Internal server error" });
